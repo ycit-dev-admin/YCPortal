246b29bb7a090384d84d6f3758a657e70a19579b
YCITDev <ycitdev@gmail.com>
2021-04-02 10:45:53 +0800

    MVC Startup 相關設定

--- PSI/Startup.cs
+++ PSI/Startup.cs
@@ -16,6 +16,7 @@ namespace PSI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();  // 似乎是可以讓該專案使用Controller的相關功能 如驗證 及 View帶有Razor Page開發風格
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +26,21 @@ namespace PSI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();  //主要是要讓專案可以讀wwwrot內的靜態檔案
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                // 似乎為定義MVC的Router的方式
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
