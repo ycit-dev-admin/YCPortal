30c277bd4f1a059def84741b1c3090e95d891c47
YCITDev <ycitdev@gmail.com>
2021-09-26 11:25:25 +0800

    增加Sysconfig Area設計

---
+++ PSI/Areas/SysConfig/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PSI.Areas.SysConfig.Models;
+
+namespace PSI.Areas.SysConfig.Controllers
+{
+    [Area("SysConfig")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult CustomerInfo()
+        {
+            var pageModel = new List<VM_Customer_Info>();
+            return View(pageModel);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCustomerInfo()
+        {
+            var pageModel = new VM_Customer_Info();
+            return View(pageModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomerInfo(VM_Customer_Info pageModel)
+        {
+            return RedirectToAction("CustomerInfo");
+            //return View(pageModel);
+        }
+    }
+}
---
+++ PSI/Areas/SysConfig/Models/VM_Customer_Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PSI.Areas.SysConfig.Models
+{
+    public class VM_Customer_Info
+    {
+        // Page
+        public string P_Contract { get; set; }
+        public string P_Car { get; set; }
+
+        // Entity
+        public string CompanyName { get; set; }
+        public string CustomerName { get; set; }
+        public string TaxId { get; set; }
+        public string Title { get; set; }
+        public string PsiType { get; set; }
+        //public bool IsContract { get; set; }
+        public string Remark { get; set; }
+        public string UpdateEmpNo { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+    }
+}
