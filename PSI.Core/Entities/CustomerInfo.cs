822e0197a6ae53d3710d13729e106ca5ce7eb368
YCITDev <ycitdev@gmail.com>
2021-09-27 21:28:03 +0800

    db changed

--- PSI.Core/Entities/CustomerInfo.cs
+++ PSI.Core/Entities/CustomerInfo.cs
@@ -10,6 +10,8 @@ namespace PSI.Core.Entities
         public string TaxId { get; set; }
         public string Title { get; set; }
         public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string ContentInfo { get; set; }
         public string PsiType { get; set; }  // 進出貨類型的客戶(1:進貨 2:出貨 3:Both)
         public string IsEffective { get; set; }
         public bool IsContract { get; set; }
---
+++ PSI.Core/Migrations/20210926034406_CustomerInfo_adj.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSI.Core.Migrations
+{
+    public partial class CustomerInfo_adj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "CustomerInfos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentInfo",
+                table: "CustomerInfos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "CustomerInfos");
+
+            migrationBuilder.DropColumn(
+                name: "ContentInfo",
+                table: "CustomerInfos");
+        }
+    }
+}
