000e0ae70f90ba78b4e61f6c4880499500652ee3
YCITDev <ycitdev@gmail.com>
2022-05-18 00:42:31 +0800

    需求_人員維護相關DB Table欄位異動

--- PSI.Core/Entities/Identity/AppUser.cs
+++ PSI.Core/Entities/Identity/AppUser.cs
@@ -4,8 +4,9 @@ namespace PSI.Core.Entities.Identity
 {
     public class AppUser : IdentityUser
     {
-        public string NickName { get; set; }
-        public string FacSite { get; set; }
-        //  public string EMPLOYEE_NO { get; set; }
+        public string NICK_NAME { get; set; }
+        public string FAC_SITE { get; set; }
+        public string EMPLOYEE_NO { get; set; }
+        public int AUTHORITY_LEVEL { get; set; }  //  0:預設權限   1: 進階權限  2:管理1權限  3:管理2權限   9:管理員權限
     }
 }
---
+++ PSI.Core/Migrations/Identity/20220514001725_add extention fields.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSI.Core.Migrations.Identity
+{
+    public partial class addextentionfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacSite",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AUTHORITY_LEVEL",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EMPLOYEE_NO",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FAC_SITE",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NICK_NAME",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AUTHORITY_LEVEL",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EMPLOYEE_NO",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FAC_SITE",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NICK_NAME",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FacSite",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
